1146e642514984fd6c7657d8927da95e8559b5cd
cobyzim <cobyzimmerman.home@gmail.com>
2022-12-14 15:13:19 -0700

    Added a lot of security things including password encoding, authorization, and authentication

--- SpringCarInventoryManager/src/main/java/com/cognixia/jump/config/SecurityConfig.java
+++ SpringCarInventoryManager/src/main/java/com/cognixia/jump/config/SecurityConfig.java
@@ -1,4 +1,71 @@
 package com.cognixia.jump.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration  //indicates class has @Bean definition methods
 public class SecurityConfig {
+	
+	@Autowired
+	UserDetailsService userDetailsService;  // Spring will look for an implementation of userDetailsService and find ours
+	
+	@Bean
+	protected UserDetailsService userDetailsService() { 
+		
+		return userDetailsService;
+	}
+	
+	// Handles authorization
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.csrf().disable()
+				.authorizeHttpRequests()
+				.antMatchers("/authenticate").permitAll()
+				.antMatchers(HttpMethod.POST, "/api/user").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		return http.build();
+	}
+	
+	// encode/decode all user passwords
+	@Bean
+	protected PasswordEncoder encoder() {
+		
+		// use BCrypt library to encode password
+		return new BCryptPasswordEncoder();
+	}
+	
+	// load the encoder & user details service that are needed for spring security to do authentication
+	@Bean
+	protected DaoAuthenticationProvider authenticationProvider() {
+		
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder(encoder());
+		
+		return authProvider;
+	}
+	
+	// can autowire and access the authentication manager (manages authentication (login) of our project)
+	@Bean
+	protected AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
+	
+	
 }
--- SpringCarInventoryManager/src/main/java/com/cognixia/jump/controller/AuthenticationController.java
+++ SpringCarInventoryManager/src/main/java/com/cognixia/jump/controller/AuthenticationController.java
@@ -1,5 +1,61 @@
 package com.cognixia.jump.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognixia.jump.model.AuthenticationRequest;
+import com.cognixia.jump.model.AuthenticationResponse;
+import com.cognixia.jump.util.JwtUtil;
+
+@RestController
 public class AuthenticationController {
+	
+	// Authentication manager -> validats/authenticates user credentials
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	UserDetailsService userDetailsService; // Spring will look for an implementation of userDetailsService and find ours
+	
+	@Autowired
+	JwtUtil jwtUtil;
+	
+	// create token at http://localhost:8080/authenticate
+	// send the username & password to try and generate a token as a response
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> createJwtToken(@RequestBody AuthenticationRequest request) throws Exception {
+		
+		// try to catch the exception for bad credentials, just so we can set our own
+		// message when this doesn't work
+		try {
+			// first make sure we have valid user checking username and password
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+		}
+		catch(BadCredentialsException e) {
+			throw new Exception("Incorrect username or password");
+		}
+		
+		// if no exception is thrown, user is valid
+		
+		// load in user details
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+		
+		// generate token for user
+		final String jwt = jwtUtil.generateTokens(userDetails);
+		
+		// return token
+		return ResponseEntity.status(201).body( new AuthenticationResponse(jwt));
+		
+	}
+	
 
 }
