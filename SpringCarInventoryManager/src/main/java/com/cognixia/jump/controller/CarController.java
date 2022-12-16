6b94e30376009eec8838e1d0c2dcbe79c28f6d5b
cobyzim <cobyzimmerman.home@gmail.com>
2022-12-16 11:37:19 -0700

    added global exception handler

--- SpringCarInventoryManager/src/main/java/com/cognixia/jump/controller/CarController.java
+++ SpringCarInventoryManager/src/main/java/com/cognixia/jump/controller/CarController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cognixia.jump.exception.ResourceNotFoundException;
 import com.cognixia.jump.model.Car;
 import com.cognixia.jump.repository.CarRepository;
 
@@ -40,13 +41,13 @@ public class CarController {
 	
 	@CrossOrigin
 	@GetMapping("/cars/{id}")
-	public ResponseEntity<?> getCar(@PathVariable int id){
+	public ResponseEntity<?> getCar(@PathVariable int id) throws ResourceNotFoundException {
 		Optional<Car> carOpt = repo.findById(id);
 		
 		if(carOpt.isPresent()) {
 			return ResponseEntity.status(200).body(carOpt.get());
 		} else {
-			return ResponseEntity.status(404).body("Car with id = " + id + " not found");
+			throw new ResourceNotFoundException("Car with id = " + id + " not found.");
 		}
 	}
 	
@@ -59,26 +60,26 @@ public class CarController {
 	
 	@CrossOrigin
 	@PutMapping("/cars/update")
-	public ResponseEntity<?> updateCar(@RequestBody Car updatedCar){
+	public ResponseEntity<?> updateCar(@RequestBody Car updatedCar) throws ResourceNotFoundException {
 		Optional<Car> found = repo.findById(updatedCar.getId());
 		
 		if (found.isPresent()) {
 			return ResponseEntity.status(202).body(repo.save(updatedCar));
 		} else {
-			return ResponseEntity.status(404).body("Car with id = " + updatedCar.getId() + " not found.");
+			throw new ResourceNotFoundException("Car with id = " + updatedCar.getId() + " not found and couldn't be updated.");
 		}
 	}
 	
 	@CrossOrigin
 	@DeleteMapping("/cars/delete/{id}")
-	public ResponseEntity<?> deleteCar(@PathVariable int id){
+	public ResponseEntity<?> deleteCar(@PathVariable int id) throws ResourceNotFoundException {
 		Optional<Car> found = repo.findById(id);
 		
 		if (found.isPresent()) {
 			repo.deleteById(id);
 			return ResponseEntity.status(202).body("Car deleted.");
 		} else {
-			return ResponseEntity.status(404).body("Car with id = " + id + " not found." );
+			throw new ResourceNotFoundException("Car with id = " + id + " not foun and couldn't be deleted.");
 		}
 	}
 	
